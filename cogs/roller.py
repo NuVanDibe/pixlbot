65a84ebd254b694b657187e3ad8a2fa81e380f2b
NuVanDibe <nuvandibe@gmail.com>
2020-07-27 12:33:39 -0500

    Added roller.py

---
+++ cogs/roller.py
@@ -0,0 +1,30 @@
+import random
+
+from discord.ext import commands
+
+
+class DiceRoll(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot  # why
+
+    @commands.command()
+    async def roll(self, ctx, *args):
+        st = ''.join(args)
+        total = 'Rolling ' + st + ": **"
+        mod = 0
+        if st.find('+', 0, -1) != -1:
+            mod = int(st.split('+')[1])
+            st = st.split('+')[0]
+        if st.find('d', 0, -1) != -1:
+            rolls = int(st.split('d')[0])
+            die = range(1, int(st.split('d')[1]) + 1)
+            dice = random.choices(die, k=rolls)
+            total = total + str(sum(dice) + mod) + "** " + str(dice) + " + " + str(mod)
+        else:
+            total = st + " and also maybe specify some dice next time"
+        await ctx.send(total)
+
+
+def setup(bot):
+    bot.add_cog(DiceRoll(bot))
